c36d7db0846fc08ac18f142ef1f9e3fb4a303c0b
Jenny Liang <jenny@Greenys-Macbook.local>
2016-11-12 16:31:26 -0800

    chrome extensions added

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,9 +1,25 @@
-console.log("hi")
-
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('submitbutton');
     // onClick's logic below:
     link.addEventListener('click', function() {
-        console.log('xxx');
+        myurl = document.getElementById("url").value
+        mypsw = document.getElementById("password").value
+        myuserid = document.getElementById("userid").value
+        console.log(myurl)
+        console.log(mypsw)
+        console.log(myuserid)
     });
 });
+
+
+function storeUserPrefs() {
+    var key = "myKey",
+        testPrefs = JSON.stringify({
+            'val': 10
+        });
+    var jsonfile = {};
+    jsonfile[key] = testPrefs;
+    chrome.storage.sync.set(jsonfile, function () {
+        console.log('Saved', key, testPrefs);
+    });
+}
